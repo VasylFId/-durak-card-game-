ef557f98af6f571dcf201f11c235986563405cab
VasylFId <157602750+VasylFId@users.noreply.github.com>
2024-07-10 20:26:41 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_socketio import SocketIO
+from flask_migrate import Migrate
 from config import Config
 
 db = SQLAlchemy()
@@ -11,6 +12,7 @@ login_manager = LoginManager()
 login_manager.login_view = 'main.login'
 login_manager.login_message_category = 'info'
 socketio = SocketIO()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
@@ -20,8 +22,9 @@ def create_app():
     bcrypt.init_app(app)
     login_manager.init_app(app)
     socketio.init_app(app)
+    migrate.init_app(app, db)
     
-    from app import routes
-    app.register_blueprint(routes.bp)
+    from app.routes import bp as main_bp
+    app.register_blueprint(main_bp)
     
     return app
