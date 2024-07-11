23972fe88a6472647549c3f0c56611e8a624dbd7
VasylFId <157602750+VasylFId@users.noreply.github.com>
2024-07-11 20:34:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
-
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://localhost/durak_db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'b1aef80f4d7e1c8f20a7063f8304eac3'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:92473Vas@localhost/durak_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
