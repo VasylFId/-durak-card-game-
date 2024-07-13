9dd367b57b72ada2415a111e6adfc47bcd700174
VasylFId <157602750+VasylFId@users.noreply.github.com>
2024-07-13 17:46:22 +0300

    Update routes.py
    Implemented routes for handling password reset requests and password resets. Added the following routes:
    /reset_password: For users to request a password reset by providing their email.
    /reset_password/<token>: For users to reset their password using a token sent to their email.
    Temporarily commented out these routes due to missing dependencies.

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from app import db, bcrypt
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm, ForgotPasswordForm, ResetPasswordForm
 from app.models import User
 from flask_login import login_user, current_user, logout_user, login_required
+# from app.utils import send_reset_email
 
 bp = Blueprint('main', __name__)
 
@@ -63,3 +64,33 @@ def logout():
 @login_required
 def game():
     return render_template('game.html', title='Game')
+
+@bp.route("/forgot_password", methods=['GET', 'POST'])
+def forgot_password():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    form = ForgotPasswordForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('main.login'))
+    return render_template('forgot_password.html', title='Forgot Password', form=form)
+
+@bp.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('main.forgot_password'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated! You are now able to log in', 'success')
+        return redirect(url_for('main.login'))
+    return render_template('reset_token.html', title='Reset Password', form=form)
