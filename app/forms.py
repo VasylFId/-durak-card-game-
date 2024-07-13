353020996c4a4aeeef18f7aca23ce99febd7b2db
VasylFId <157602750+VasylFId@users.noreply.github.com>
2024-07-13 17:46:01 +0300

    Update forms.py
    Added forms for handling password reset requests and password resets. Specifically, added RequestResetForm and ResetPasswordForm to handle user input for the password reset functionality.

--- app/forms.py
+++ app/forms.py
@@ -20,8 +20,25 @@ class RegistrationForm(FlaskForm):
         if user:
             raise ValidationError('That email is already registered. Please choose a different one.')
 
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class ForgotPasswordForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('There is no account with that email. You must register first.')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset Password')
