20b20e8f3e688303773937dc2ced5bc03f927113
VasylFId <157602750+VasylFId@users.noreply.github.com>
2024-10-28 11:13:34 +0000

    Update test_deck.py

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -14,25 +14,32 @@ from game_logic.card_package import Deck, Card
 class TestDeck(unittest.TestCase):
 
     def setUp(self):
+        """Initialize a new Deck instance for each test."""
+
         self.deck = Deck()
 
     def test_deck_initialization(self):
         """Test that the deck initializes with the correct number of
         cards and sets a trump card."""
+
         self.assertEqual(len(self.deck), 35)  # 36 cards, 1 is trump
         self.assertIsInstance(self.deck.trump_card, Card)
         self.assertFalse(self.deck._Deck__trump_card_drawn)
 
     def test_draw_card(self):
         """Test drawing a card from the deck reduces the deck size."""
+
         initial_size = len(self.deck)
         card = self.deck.draw_card()
+
+        # Check that the card is not None and the deck size is reduced
         self.assertEqual(len(self.deck), initial_size - 1)
         self.assertIsInstance(card, Card)
 
     def test_draw_all_cards(self):
         """Test drawing all cards including the trump card leaves the
         deck empty."""
+
         while self.deck:  # Draw all but one (trump card) from the deck.
             self.deck.draw_card()
 
@@ -47,6 +54,7 @@ class TestDeck(unittest.TestCase):
     def test_draw_trump_card(self):
         """Test that drawing the trump card sets the flag and returns
         the correct card."""
+
         deck_trump_card = self.deck.trump_card
         trump_card = self.deck.draw_trump_card()
 
@@ -63,6 +71,7 @@ class TestDeck(unittest.TestCase):
     def test_return_card_to_deck(self):
         """Test returning a card to the deck increases its size and
         shuffles correctly."""
+
         card = self.deck.draw_card()
         initial_size = len(self.deck)
 
@@ -74,6 +83,7 @@ class TestDeck(unittest.TestCase):
 
     def test_set_trump_card(self):
         """Test that setting a new trump card works as expected."""
+
         initial_trump = self.deck.trump_card
         self.deck.set_trump_card()
         new_trump = self.deck.trump_card
