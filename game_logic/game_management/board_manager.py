69d573831b91aa345bf7050f443ae93669fb74d7
VasylFId <157602750+VasylFId@users.noreply.github.com>
2024-10-28 18:37:43 +0000

    Update board_manager.py

--- game_logic/game_management/board_manager.py
+++ game_logic/game_management/board_manager.py
@@ -1,3 +1,21 @@
+"""
+This module contains the BoardManager class, which is responsible for managing
+the board in the game.
+
+Classes:
+    BoardManager: A class for managing the board in the game.
+
+Usage:
+    board_manager = BoardManager(trump_card)
+    board_manager.add_card_to_board(card)
+    board_manager.get_board_state()
+    board_manager.get_board_ranks()
+    board_manager.clear_board()
+    board_manager.is_board_full()
+    board_manager.next_round()
+"""
+
+
 import logging
 from game_logic.card_package import Card
 
@@ -5,19 +23,53 @@ logger = logging.getLogger(__name__)
 
 
 class BoardManager:
-    def __init__(self, trump_card, initial_round: int = 1):
-        self.board = []
-        self.trump_card = trump_card
-        self.round_number = initial_round
 
-    def add_card_to_board(self, card: Card):
+    """
+    A class for managing the board in the game.
+
+    Attributes:
+        board (list of Card): The list of cards on the board.
+        trump_card (Card): The trump card for the game.
+        round_number (int): The current round number.
+
+    Methods:
+        add_card_to_board(card: Card) -> None:
+            Adds a card to the board.
+        get_board_state() -> list:
+            Returns the current state of the board.
+        get_board_ranks() -> set:
+            Returns a set of the ranks of all cards on the board.
+        clear_board() -> None:
+            Clears the board at the end of the round.
+        is_board_full() -> bool:
+            Checks if the board is full based on the current round.
+        next_round() -> None:
+            Prepares the board for the next round.
+    """
+
+    def __init__(self, trump_card, initial_round: int = 1):
+        """
+        Initializes the board with an empty list of cards, a trump card, and
+        the initial round number.
+
+        Args:
+            trump_card (Card): The trump card for the game.
+            initial_round (int): The initial round number.
+        """
+
+        self.__board = []
+        self.__trump_card = trump_card
+        self.__round_number = initial_round
+
+    def add_card_to_board(self, card: Card) -> None:
         """
         Adds a card to the board.
 
         Args:
             card (Card): The card to be added to the board.
         """
-        self.board.append(card)
+
+        self.__board.append(card)
         logger.info(f"Card {card} added to the board.")
 
     def get_board_state(self) -> list:
@@ -27,7 +79,8 @@ class BoardManager:
         Returns:
             list: The current state of the board.
         """
-        return self.board
+
+        return self.__board
 
     def get_board_ranks(self) -> set:
         """
@@ -36,13 +89,15 @@ class BoardManager:
         Returns:
             set: A set of ranks currently on the board.
         """
-        return {card.rank for card in self.board}
 
-    def clear_board(self):
+        return {card.rank for card in self.__board}
+
+    def clear_board(self) -> None:
         """
         Clears the board at the end of the round.
         """
-        self.board = []
+
+        self.__board = []
 
     def is_board_full(self) -> bool:
         """
@@ -51,8 +106,8 @@ class BoardManager:
         Returns:
             bool: True if the board is full, False otherwise.
         """
-        max_cards = 10 if self.round_number == 1 else 12
-        return len(self.board) >= max_cards
+        max_cards = 10 if self.__round_number == 1 else 12
+        return len(self.__board) >= max_cards
 
     def next_round(self):
         """
@@ -60,5 +115,27 @@ class BoardManager:
         incrementing the round number.
         """
         self.clear_board()
-        self.round_number += 1
-        logger.info(f"Round {self.round_number} begins.")
+        self.__round_number += 1
+        logger.info(f"Round {self.__round_number} begins.")
+
+    @property
+    def round_number(self):
+        """
+        Property that returns the current round number.
+
+        Returns:
+            int: The current round number.
+        """
+
+        return self.__round_number
+
+    @property
+    def trump_card(self):
+        """
+        Property that returns the trump card.
+
+        Returns:
+            Card: The trump card.
+        """
+
+        return self.__trump_card
