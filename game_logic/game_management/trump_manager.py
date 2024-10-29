1b1d06363654d1067a6318c8333f0249e868844b
VasylFId <157602750+VasylFId@users.noreply.github.com>
2024-10-29 13:01:21 +0000

    Update trump_manager.py

--- game_logic/game_management/trump_manager.py
+++ game_logic/game_management/trump_manager.py
@@ -1,3 +1,17 @@
+"""
+This module contains the TrumpManager class, which is responsible for managing
+the trump card in the game.
+
+Classes:
+    TrumpManager: A class for managing the trump card.
+
+Usage:
+    trump_manager = TrumpManager()
+    trump_card_valid = trump_manager.is_trump_card_valid(deck, players)
+    new_trump_card = trump_manager.set_new_trump_card(deck, players)
+"""
+
+
 import logging
 from game_logic.card_package import Card, Deck, Rank
 
@@ -5,12 +19,26 @@ logger = logging.getLogger(__name__)
 
 
 class TrumpManager:
+    """
+    A class for managing the trump card.
+
+    Methods:
+        __trump_card_is_ace(trump_card: Card) -> bool:
+            Checks if the current trump card is an Ace.
+        __players_have_trump(players: list, trump_card: Card) -> bool:
+            Checks if any player has a card of the trump suit in their hand.
+        is_trump_card_valid(deck: Deck, players: list) -> bool:
+            Checks if the current trump card is valid.
+        set_new_trump_card(deck: Deck, players: list) -> Card:
+            Sets a new trump card by drawing the last card in the deck.
+    """
 
     @staticmethod
     def __trump_card_is_ace(trump_card: Card) -> bool:
         """
         Checks if the current trump card is an Ace.
         """
+
         return trump_card.rank == Rank.ACE
 
     @staticmethod
@@ -44,6 +72,7 @@ class TrumpManager:
         Returns:
             bool: True if the trump card is valid, False otherwise.
         """
+
         trump_card = deck.trump_card
 
         return not TrumpManager.__trump_card_is_ace(trump_card) and \
@@ -60,6 +89,7 @@ class TrumpManager:
         Returns:
             Card: The new trump card.
         """
+
         while not TrumpManager.is_trump_card_valid(deck, players):
             deck.set_trump_card()
             new_trump_card = deck.trump_card
