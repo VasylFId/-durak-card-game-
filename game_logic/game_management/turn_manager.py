5038f53c669e9bec47d7a2440f3a26a0f08f1c8a
VasylFId <157602750+VasylFId@users.noreply.github.com>
2024-11-01 11:42:20 +0000

    Update turn_manager.py

--- game_logic/game_management/turn_manager.py
+++ game_logic/game_management/turn_manager.py
@@ -1,21 +1,56 @@
+"""
+This module contains the TurnManager class, which is responsible for managing
+the turns in the game.
+
+Classes:
+    TurnManager: A class for managing the turns in the game.
+
+Usage:
+    turn_manager = TurnManager(attacker, defender, board_manager)
+    turn_manager.execute_attack(card)
+    turn_manager.handle_defense(attack_card, defense_card)
+    turn_manager.update_turn_state()
+"""
+
+
 import logging
 from game_logic.card_package import Card
 from game_logic.players import Player
+from game_logic.game_management import BoardManager
 from game_logic.game_management import PlayerManager, RulesManager
 
 logger = logging.getLogger(__name__)
 
 
 class TurnManager:
-    def __init__(self, attacker: Player, defender: Player, board_manager):
+    """
+    A class for managing the turns in the game.
+
+    Methods:
+        switch_turn():
+            Switches the turn between the attacking and defending players.
+        execute_attack(card):
+            Handles the logic for the attacking player to make a move.
+        handle_defense(attack_card, defense_card):
+            Handles the logic for the defending player to respond to the attack
+        update_turn_state():
+            Updates the state of the turn, tracking which player is active and
+            what actions have been taken.
+    """
+
+    def __init__(self, attacker: Player, defender: Player,
+                 board_manager: BoardManager):
         """
-        Initializes the TurnManager with the current attacking and defending players.
+        Initializes the TurnManager with the current attacking and defending
+        players.
 
         Args:
             attacker (Player): The player who will attack first.
             defender (Player): The player who will defend first.
-            board_manager (BoardManager): The manager that holds the current state of the board.
+            board_manager (BoardManager): The manager that holds the current
+            state of the board.
         """
+
         self.attacker = attacker
         self.defender = defender
         self.is_attacker_turn = True
@@ -28,74 +63,117 @@ class TurnManager:
         }
         self.board_manager = board_manager
 
-    def switch_turn(self):
+    def switch_turn(self) -> None:
         """
         Switches the turn between the attacking and defending players.
         """
+
+        # Switch turns only if the attack or defense has been completed
         if self.attack_completed or self.defense_completed:
+
+            # Switch turns
             self.is_attacker_turn = not self.is_attacker_turn
             self.is_defender_turn = not self.is_defender_turn
+
+            # Update the current attacker and defender
             turn = 'Attacker' if self.is_attacker_turn else 'Defender'
             logger.info(f"Turn switched: {turn}'s turn")
+
+            # Reset the attack and defense flags
             self.attack_completed = False
             self.defense_completed = False
 
-    def execute_attack(self, card: Card):
+    def execute_attack(self, card: Card) -> bool:
         """
         Handles the logic for the attacking player to make a move.
 
         Args:
             card (Card): The card played by the attacker.
+
+        Returns:
+            bool: True if the attack is successful, False otherwise.
         """
+
+        # Check if it's the attacker's turn
         if not self.is_attacker_turn:
             logger.warning("It's not the attacker's turn!")
             return False
 
         # Check if the attack is valid according to the game rules
         if RulesManager.is_valid_attack(self.board_manager, card):
+
             # Attacker plays a card
             PlayerManager.remove_card_from_hand(self.attacker, card)
+
+            # Add the card to the board
             self.turn_state["attacks"].append(card)
             self.board_manager.add_card_to_board(card)
+
             logger.info(f"{self.attacker.name} attacks with {card}")
+
+            # Update the turn state
             self.attack_completed = True
-            self.switch_turn()  # Switch to defender's turn
+
+            # Switch to defender's turn
+            self.switch_turn()
+
             return True
         else:
-            logger.warning(f"{self.attacker.name}'s attack with {card} is invalid.")
+            logger.warning(f"{self.attacker.name}'s attack with {card}" +
+                           " is invalid.")
             return False
 
-    def handle_defense(self, attack_card: Card, defense_card: Card):
+    def handle_defense(self, attack_card: Card, defense_card: Card) -> bool:
         """
         Handles the logic for the defending player to respond to the attack.
 
         Args:
             defense_card (Card): The card played by the defender.
+            attack_card (Card): The card played by the attacker.
+
+        Returns:
+            bool: True if the defense is successful, False otherwise.
         """
+
+        # Check if it's the defender's turn
         if not self.is_defender_turn:
             logger.warning("It's not the defender's turn!")
             return False
 
+        # Check if the defender has a valid defense
         if not self.turn_state["attacks"]:
             logger.warning("No attack to defend against!")
             return False
 
         # Check if the defense is valid according to the game rules
-        if RulesManager.is_valid_defense(self.board_manager, attack_card, defense_card):
+        if RulesManager.is_valid_defense(self.board_manager,
+                                         attack_card, defense_card):
+
             # Defender plays a card
             PlayerManager.remove_card_from_hand(self.defender, defense_card)
+
+            # Add the card to the board
             self.turn_state["defenses"].append(defense_card)
+
+            # Add the card to the board
             self.board_manager.add_card_to_board(defense_card)
             logger.info(f"{self.defender.name} defends with {defense_card}")
+
+            # Update the turn state
             self.defense_completed = True
-            self.switch_turn()  # Switch to attacker's turn
+
+            # Switch to attacker's turn
+            self.switch_turn()
+
             return True
         else:
-            logger.warning(f"{self.defender.name}'s defense with {defense_card} is invalid.")
+            logger.warning(f"{self.defender.name}'s defense with " +
+                           f"{defense_card} is invalid.")
             return False
 
     def update_turn_state(self):
         """
-        Updates the state of the turn, tracking which player is active and what actions have been taken.
+        Updates the state of the turn, tracking which player is active
+        and what actions have been taken.
         """
         logger.info(f"Current turn state: {self.turn_state}")
