c7898f0925bf67b7ebd528dd98a8fdda8217d7c9
VasylFId <157602750+VasylFId@users.noreply.github.com>
2025-03-19 18:14:10 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-from app import create_app, socketio, db
+from app import create_app
+from app.extensions import db, socketio
 
+# Create Flask app
 app = create_app()
 
 # Create database tables if they don't exist
@@ -7,4 +9,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
+    # Run app with SocketIO
     socketio.run(app, debug=True, host='0.0.0.0', port=5001)
