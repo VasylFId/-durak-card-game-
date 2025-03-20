8e804f429fdcef7b9e2717fc1dedb5d3a363fbe4
VasylFId <157602750+VasylFId@users.noreply.github.com>
2025-03-20 01:38:06 +0000

    Update __init__.py

--- app/game/__init__.py
+++ app/game/__init__.py
@@ -1,5 +1,7 @@
+# File path: app/game/__init__.py
+
 from flask import Blueprint
 
 bp = Blueprint('game', __name__, url_prefix='/game')
 
-from app.game import routes, events
+from app.game import routes
